8620c7cae443236f058e7f116827e7c023b20bdb
saraboutej <sboutej@exoplatform.com>
2022-02-23 16:18:29 +0100

    Task-54473-54462: Fix show more avatar position on task assignee list

--- task-management/src/main/webapp/vue-app/tasks-management/components/ProjectTasks/TaskViewCard.vue
+++ task-management/src/main/webapp/vue-app/tasks-management/components/ProjectTasks/TaskViewCard.vue
@@ -62,7 +62,6 @@
                 :users="avatarToDisplay"
                 :max="1"
                 :icon-size="26"
-                avatar-overlay-position
                 @open-detail="$root.$emit('displayTasksAssigneeAndCoworker', assigneeAndCoworkerArray)" />
             </div>
           </div>
