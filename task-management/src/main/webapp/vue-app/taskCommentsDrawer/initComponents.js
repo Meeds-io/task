cde7e3ba70f0c5a21f6ccb639df0df7711442ada
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2022-02-24 12:52:39 +0100

    TASK-54447: Place task comments drawer and its sub compopnents inside a dedicated amd module (#14)
    Place task comments drawer and its sub compopnents inside a dedicated amd module for add the capability to re-use in other apps such as processes app which is based on the tasks app

---
+++ task-management/src/main/webapp/vue-app/taskCommentsDrawer/initComponents.js
@@ -0,0 +1,52 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ * Copyright (C) 2022 Meeds Association
+ * contact@meeds.io
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import TaskCommentsDrawer from './components/TaskCommentsDrawer.vue';
+import TaskComments from './components/TaskComments.vue';
+import TaskCommentEditor from './components/TaskCommentEditor.vue';
+import TaskCommentItem from './components/TaskCommentItem.vue';
+import TaskLastComment from './components/TaskLastComment.vue';
+import viewAllComments from './components/ViewAllComments.vue';
+
+import * as tasksService from '../../js/tasksService.js';
+import * as taskDrawerApi from '../../js/taskDrawerApi.js';
+
+
+const components = {
+  'task-comment-editor': TaskCommentEditor,
+  'task-comments': TaskComments,
+  'task-comments-drawer': TaskCommentsDrawer,
+  'task-comment-item': TaskCommentItem,
+  'task-last-comment': TaskLastComment,
+  'view-all-comments': viewAllComments,
+};
+
+for (const key in components) {
+  Vue.component(key, components[key]);
+}
+
+if (!Vue.prototype.$tasksService) {
+  window.Object.defineProperty(Vue.prototype, '$tasksService', {
+    value: tasksService,
+  });
+}
+
+if (!Vue.prototype.$taskDrawerApi) {
+  window.Object.defineProperty(Vue.prototype, '$taskDrawerApi', {
+    value: taskDrawerApi,
+  });
+}
