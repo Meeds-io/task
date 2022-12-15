bb2aa2bf49685dd9e00307d23ab99b1b5be34dd2
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2022-12-15 13:33:12 +0100

    feat: Engagement Center Access to automatic/manuel rules detail - MEED-1262 - Meeds-io/MIPs#13 (#149)
    Allow users to access rule detail via a drawer in the program detail

--- task-management/src/main/webapp/vue-app/engagementCenterExtensions/initComponents.js
+++
@@ -1,9 +0,0 @@
-import TaskActionValue from './components/TaskActionValue.vue';
-const components = {
-  'task-action-value': TaskActionValue,
-};
-
-for (const key in components) {
-  Vue.component(key, components[key]);
-}
-
