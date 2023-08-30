0b9a73a79de1441ac8aef35c92e24448b2d6369a
saraboutej <sboutej@exoplatform.com>
2023-08-30 13:29:44 +0100

    Add license header

--- services/src/test/java/org/exoplatform/task/integration/TaskAttachmentPluginTest.java
+++ services/src/test/java/org/exoplatform/task/integration/TaskAttachmentPluginTest.java
@@ -1,3 +1,21 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2023 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.exoplatform.task.integration;
 
 import org.exoplatform.commons.exception.ObjectNotFoundException;
@@ -19,6 +37,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
+
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.HashSet;
