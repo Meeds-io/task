df7d49c8f9ca81bafda5a19b13021dba67253bc1
saraboutej <sboutej@exoplatform.com>
2023-09-20 17:49:59 +0100

    feat: Attaching images to a task I create is not possible - MEED-2546 - Meeds-io/MIP#53

--- webapps/src/main/webapp/vue-app/taskDrawer/components/TaskDrawerComponents/TaskDescriptionEditor.vue
+++ webapps/src/main/webapp/vue-app/taskDrawer/components/TaskDrawerComponents/TaskDescriptionEditor.vue
@@ -87,7 +87,7 @@ export default {
       inputVal: '',
       editorReady: false,
       showEditor: false,
-      savingDescription: false,
+      savingDescription: false
     };
   },
   computed: {
@@ -147,7 +147,7 @@ export default {
     });
     document.addEventListener('onAddTask', () => {
       this.$emit('addTaskDescription',this.value);
-      this.hideDescriptionEditor();
+      this.showEditor = false;
     });
     this.inputVal = this.value || '';
     $('#task-Drawer').on('click', (event) => {
@@ -205,7 +205,7 @@ export default {
       } else if (event?.target?.nodeName === 'A') {
         window.open(event.target.href, '_blank');
       }
-    }
+    },
   }
 };
 </script>
