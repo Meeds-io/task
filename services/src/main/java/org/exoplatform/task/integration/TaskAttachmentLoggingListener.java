e45c362f87c481aeb246c9a6adbe757d37319675
saraboutej <sboutej@exoplatform.com>
2023-09-21 15:30:59 +0100

    PR review

--- services/src/main/java/org/exoplatform/task/integration/TaskAttachmentLoggingListener.java
+++ services/src/main/java/org/exoplatform/task/integration/TaskAttachmentLoggingListener.java
@@ -1,3 +1,21 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2023 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.exoplatform.task.integration;
 
 import org.exoplatform.services.listener.Event;
@@ -20,12 +38,8 @@ public class TaskAttachmentLoggingListener extends Listener<String, ObjectAttach
     String username = event.getSource();
     ObjectAttachmentId objectAttachment = event.getData();
 
-    if (objectAttachment == null) {
-      return;
+    if (objectAttachment != null && ATTACHMENT_TASK_OBJECT_TYPE.equals(objectAttachment.getObjectType())) {
+      taskService.addTaskLog(Long.parseLong(objectAttachment.getObjectId()), username, "attach_image", "");
     }
-    if (!objectAttachment.getObjectType().equals(ATTACHMENT_TASK_OBJECT_TYPE)) {
-      return;
-    }
-    taskService.addTaskLog(Long.parseLong(objectAttachment.getObjectId()), username, "attach_image", "");
   }
 }
