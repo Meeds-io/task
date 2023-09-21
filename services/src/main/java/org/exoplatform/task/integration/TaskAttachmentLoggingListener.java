9c16238831ec550bcdf7359b35611a42f3b79f85
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2023-09-21 15:39:29 +0100

    feat:Add an information in task history when adding images - MEED-2437 - Meeds-io/MIPs#53 (#256)
    This PR adds an new log to the task changes drawer when attaching a new image to the task description.

---
+++ services/src/main/java/org/exoplatform/task/integration/TaskAttachmentLoggingListener.java
@@ -0,0 +1,45 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2023 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.exoplatform.task.integration;
+
+import org.exoplatform.services.listener.Event;
+import org.exoplatform.services.listener.Listener;
+import org.exoplatform.social.attachment.model.ObjectAttachmentId;
+import org.exoplatform.task.service.TaskService;
+
+public class TaskAttachmentLoggingListener extends Listener<String, ObjectAttachmentId> {
+
+  public static final String ATTACHMENT_TASK_OBJECT_TYPE = "task";
+
+  private final TaskService taskService;
+
+  public TaskAttachmentLoggingListener(TaskService taskService) {
+    this.taskService = taskService;
+  }
+
+  @Override
+  public void onEvent(Event<String, ObjectAttachmentId> event) throws Exception {
+    String username = event.getSource();
+    ObjectAttachmentId objectAttachment = event.getData();
+
+    if (objectAttachment != null && ATTACHMENT_TASK_OBJECT_TYPE.equals(objectAttachment.getObjectType())) {
+      taskService.addTaskLog(Long.parseLong(objectAttachment.getObjectId()), username, "attach_image", "");
+    }
+  }
+}
