63b5f01f7d5c9e1a8c9b5e9a82a7577d62406cef
saraboutej <sboutej@exoplatform.com>
2023-10-27 10:09:12 +0100

    feat: Apply widget-style to  tasks app (external-stream) - MEED-2763 - Meeds-io/MIPs#81

---
+++ webapps/src/main/webapp/vue-app/tasks/components/TaskEmptyRow.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="mx-auto text-center">
+    <v-icon class="icon-default-color mb-3" large>fas fa-tasks</v-icon>
+    <p class="mb-5 subtitle-1">{{ $t('tasks.label.allSet') }}</p>
+    <v-btn class="btn btn-primary d-inline" @click="$emit('open-task-drawer')">{{ $t('label.addTask') }}</v-btn>
+  </div> 
+</template>
+<script>
+export default {
+};
+</script>
