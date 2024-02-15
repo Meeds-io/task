7731d12e89c912cd0ce6568be2c746789e458222
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2024-02-15 14:30:40 +0100

    feat: Ability to choose multiple projects (tasks events) - MEED-3351 - Meeds-io/MIPs#105 (#357)
    This PR will allow the admin to choose multiple projects when creating
    an action

---
+++ webapps/src/main/webapp/vue-app/connectorEventExtensions/components/TaskEventProjectItem.vue
@@ -0,0 +1,54 @@
+<!--
+ This file is part of the Meeds project (https://meeds.io/).
+
+ Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+<template>
+  <v-list-item class="clickable" :href="projectLink">
+    <v-list-item-icon class="me-3 my-auto">
+      <v-icon class="primary--text">fas fa-tasks</v-icon>
+    </v-list-item-icon>
+    <v-list-item-content>
+      <v-list-item-title class="text-color body-2">
+        <p
+          class="ma-auto text-truncate"
+          v-sanitized-html="projectName"></p>
+      </v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  props: {
+    project: {
+      type: Object,
+      default: null
+    },
+  },
+  computed: {
+    projectId() {
+      return this.project?.id;
+    },
+    projectLink() {
+      return `${eXo.env.portal.context}/${eXo.env.portal.metaPortalName}/tasks/projectDetail/${this.projectId}`;
+    },
+    projectName() {
+      return this.project?.name;
+    },
+  },
+};
+</script>
