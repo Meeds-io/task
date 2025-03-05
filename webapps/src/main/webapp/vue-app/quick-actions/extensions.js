6375a696061d0cfa265cec400f10b133f10c36d1
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-03-05 10:36:17 +0100

    PoC: Add Task Drawer Quick Action Drawer Display (#469)

--- webapps/src/main/webapp/vue-app/quick-actions/extensions.js
+++ webapps/src/main/webapp/vue-app/quick-actions/extensions.js
@@ -22,5 +22,61 @@ extensionRegistry.registerExtension('QuickAction', 'Extension', {
   icon: 'fa-tasks',
   name: 'quickActions.task.name',
   description: 'quickActions.task.description',
-  keystroke: 'k',
+  click: () => new Promise(resolve => {
+    window.require(['SHARED/eXoVueI18n', 'SHARED/taskDrawer'], exoi18n => init(exoi18n, resolve));
+  }),
 });
+
+async function init(exoi18n, callback) {
+  const appId = 'task-quick-actions';
+  if (!document.querySelector(`#${appId}`)) {
+    const parent = document.createElement('div');
+    parent.id = appId;
+    document.querySelector('#vuetify-apps').appendChild(parent);
+    await initApp(appId, exoi18n);
+  }
+  document.dispatchEvent(new CustomEvent('quick-action-task-drawer'));
+  callback();
+}
+
+function initApp(appId, exoi18n) {
+  const lang = eXo.env.portal.language;
+  const url = `/task-management/i18n/locale.portlet.taskManagement?lang=${lang}`;
+  return new Promise(resolve => exoi18n.loadLanguageAsync(lang, url)
+    .then(i18n => Vue.createApp({
+      template: `
+        <task-drawer
+          id="${appId}"
+          ref="taskDrawer" />
+      `,
+      computed: {
+        isMobile() {
+          return this.$vuetify?.breakpoint?.mobile;
+        },
+      },
+      created() {
+        document.addEventListener('quick-action-task-drawer', this.openTaskDrawer);
+      },
+      mounted() {
+        resolve();
+      },
+      beforeDestroy() {
+        document.removeEventListener('quick-action-task-drawer', this.openTaskDrawer);
+      },
+      methods: {
+        openTaskDrawer() {
+          this.$refs.taskDrawer.open({
+            id: null,
+            status: {
+              project: null
+            },
+            priority: 'NONE',
+            description: '',
+            title: ''
+          });
+        },
+      },
+      vuetify: Vue.prototype.vuetifyOptions,
+      i18n,
+    }, `#${appId}`, 'Task Quick Action')));
+}
