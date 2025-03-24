8b9fac132ab2fa3772bcdc24cfd6bf1385205e55
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-03-24 10:13:08 +0100

    fix: Fix no tasks placeholder alignment - MEED-8647 - Meeds-io/meeds#3145 (#475)

--- webapps/src/main/webapp/vue-app/tasks/components/TaskEmptyRow.vue
+++ webapps/src/main/webapp/vue-app/tasks/components/TaskEmptyRow.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="mx-auto text-center">
-    <v-icon color="secondary" size="60" class="mb-3">fas fa-tasks</v-icon>
-    <p class="mb-5 subtitle-1 text-sub-title">{{ $t('label.start.adding.tasks') }}</p>
-  </div> 
+  <div class="d-flex flex-column flex-grow-1 justify-center align-center fill-height">
+    <div class="mx-auto d-flex flex-column">
+      <v-icon color="secondary" size="60" class="mb-3">fas fa-tasks</v-icon>
+      <p class="mb-5 subtitle-1 text-sub-title">{{ $t('label.start.adding.tasks') }}</p>
+    </div>
+  </div>
 </template>
