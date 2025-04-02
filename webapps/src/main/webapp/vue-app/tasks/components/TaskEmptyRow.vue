9536993e61bda34694ea16939816073a692e16e5
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-04-02 10:52:30 +0100

    fix: Unify tasks app header - MEED-8675 - Meeds-io/meeds#3173 (#480)
    This change is going to unify the task app header
    Resolves https://github.com/meeds-io/meeds/issues/3173

--- webapps/src/main/webapp/vue-app/tasks/components/TaskEmptyRow.vue
+++ webapps/src/main/webapp/vue-app/tasks/components/TaskEmptyRow.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="d-flex flex-column flex-grow-1 justify-center align-center fill-height">
-    <div class="mx-auto d-flex flex-column">
-      <v-icon color="secondary" size="60" class="mb-3">fas fa-tasks</v-icon>
-      <p class="mb-5 subtitle-1 text-sub-title">{{ $t('label.start.adding.tasks') }}</p>
-    </div>
+  <div class="mx-auto mt-n5 d-flex flex-column">
+    <v-icon color="secondary" size="60" class="mb-2">fas fa-tasks</v-icon>
+    <p class="mt-2 mb-0 text-sub-title">{{ $t('label.start.adding.tasks') }}</p>
   </div>
 </template>
