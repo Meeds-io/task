26c9b159edf62b52e6715eb4d620c35541e9b9b9
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-04-09 17:06:01 +0100

    feat: Add Task Project Drawer Quick Action - Meeds-io/MIPs#185 (#484)

--- webapps/src/main/webapp/vue-app/tasks/components/TaskEmptyRow.vue
+++ webapps/src/main/webapp/vue-app/tasks/components/TaskEmptyRow.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="mx-auto mt-n5 d-flex flex-column">
-    <v-icon color="secondary" size="60" class="mb-2">fas fa-tasks</v-icon>
+    <v-icon
+      color="secondary"
+      size="60"
+      class="mb-2">
+      fas fa-tasks
+    </v-icon>
     <p class="mt-2 mb-0 text-sub-title">{{ $t('label.start.adding.tasks') }}</p>
   </div>
 </template>
