77eade87e510f28a1df933fbfc56c02124db7083
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-05-09 18:35:29 +0100

    feat: Implement Task Editor Content Link Extensions - MEED-8937 - Meeds-io/MIPs#192 (#492)
    This change will implement Task Plugins used by Content Link Service APIs.

---
+++ webapps/src/main/java/io/meeds/task/TaskApplication.java
@@ -0,0 +1,48 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.task;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.PropertySource;
+
+import io.meeds.spring.AvailableIntegration;
+import io.meeds.spring.kernel.PortalApplicationContextInitializer;
+
+@SpringBootApplication(scanBasePackages = {
+    "io.meeds.task",
+    AvailableIntegration.KERNEL_MODULE,
+    AvailableIntegration.WEB_MODULE,
+  }, exclude = {
+    LiquibaseAutoConfiguration.class,
+    DataSourceAutoConfiguration.class,
+    DataSourceTransactionManagerAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+})
+@EnableCaching
+@PropertySource("classpath:application.properties")
+@PropertySource("classpath:application-common.properties")
+@PropertySource("classpath:task.properties")
+public class TaskApplication extends PortalApplicationContextInitializer {
+
+}
