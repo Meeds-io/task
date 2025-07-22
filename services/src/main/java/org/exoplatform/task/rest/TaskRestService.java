47031c3b5b2903f495b3023c1326de7b87538f86
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-07-22 08:37:52 +0100

    feat: Search Task in multiple spaces - MEED-9461 - Meeds-io/MIPs#211 (#523)

--- services/src/main/java/org/exoplatform/task/rest/TaskRestService.java
+++ services/src/main/java/org/exoplatform/task/rest/TaskRestService.java
@@ -40,6 +40,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -172,7 +173,7 @@ public class TaskRestService implements ResourceContainer {
   @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Request fulfilled") })
   public Response getTasks(@Parameter(description = "Type of task to get (all, incoming, overdue)") @QueryParam("status") String status,
                            @Parameter(description = "Search term") @QueryParam("q") String query,
-                           @Parameter(description = "Space id") @QueryParam("spaceId") String spaceId,
+                           @Parameter(description = "Space id") @QueryParam("spaceId") List<String> spaceIds,
                            @Parameter(description = "Offset") @Schema(defaultValue = "0") @QueryParam("offset") int offset,
                            @Parameter(description = "Limit") @Schema(defaultValue = "20") @QueryParam("limit") int limit,
                            @Parameter(description = "Returning the number of tasks or not") @Schema(defaultValue = "false") @QueryParam("returnSize") boolean returnSize,
@@ -231,11 +232,13 @@ public class TaskRestService implements ResourceContainer {
         }
         }
       } else {
-        if (spaceId != null) {
-          Space space = spaceService.getSpaceById(spaceId);
-          if (space != null) {
-            memberships = memberships.stream().filter(membership -> membership.contains(space.getGroupId())).toList();
-          }
+        if (CollectionUtils.isNotEmpty(spaceIds)) {
+         for (String spaceId : spaceIds) {
+           Space space = spaceService.getSpaceById(spaceId);
+           if (space != null) {
+             memberships = memberships.stream().filter(membership -> membership.contains(space.getGroupId())).toList();
+           }
+         }
         }
         tasks = taskService.findTasksByMemberShips(currentUser, memberships, query, limit);
         tasksSize = taskService.countTasks(currentUser, query, memberships);
