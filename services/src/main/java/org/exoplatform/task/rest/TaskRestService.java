9863f6522a4f6df608f4004936e3c44d001acb98
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-07-22 14:46:23 +0100

    fix: search task across multiple spaces returns all results - MEED-9461 - Meeds-io/MIPs#211 (#524)

--- services/src/main/java/org/exoplatform/task/rest/TaskRestService.java
+++ services/src/main/java/org/exoplatform/task/rest/TaskRestService.java
@@ -25,6 +25,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.TimeZone;
 import java.util.stream.Collectors;
 
@@ -173,7 +174,7 @@ public class TaskRestService implements ResourceContainer {
   @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Request fulfilled") })
   public Response getTasks(@Parameter(description = "Type of task to get (all, incoming, overdue)") @QueryParam("status") String status,
                            @Parameter(description = "Search term") @QueryParam("q") String query,
-                           @Parameter(description = "Space id") @QueryParam("spaceId") List<String> spaceIds,
+                           @Parameter(description = "Space id") @QueryParam("spaceId") List<Long> spaceIds,
                            @Parameter(description = "Offset") @Schema(defaultValue = "0") @QueryParam("offset") int offset,
                            @Parameter(description = "Limit") @Schema(defaultValue = "20") @QueryParam("limit") int limit,
                            @Parameter(description = "Returning the number of tasks or not") @Schema(defaultValue = "false") @QueryParam("returnSize") boolean returnSize,
@@ -232,14 +233,7 @@ public class TaskRestService implements ResourceContainer {
         }
         }
       } else {
-        if (CollectionUtils.isNotEmpty(spaceIds)) {
-         for (String spaceId : spaceIds) {
-           Space space = spaceService.getSpaceById(spaceId);
-           if (space != null) {
-             memberships = memberships.stream().filter(membership -> membership.contains(space.getGroupId())).toList();
-           }
-         }
-        }
+        memberships = filterMembershipsBySpaceIds(spaceIds, memberships);
         tasks = taskService.findTasksByMemberShips(currentUser, memberships, query, limit);
         tasksSize = taskService.countTasks(currentUser, query, memberships);
       }
@@ -1162,6 +1156,18 @@ public class TaskRestService implements ResourceContainer {
     if (comment != null) {
       comment.setComment(HtmlUtils.transform(comment.getComment(), new HtmlTransformerContext(userIdentity, null)));
     }
+  }
+
+  private List<String> filterMembershipsBySpaceIds(List<Long> spaceIds, List<String> memberships) {
+    if (CollectionUtils.isNotEmpty(spaceIds)) {
+      List<String> spacesGroupId = spaceIds.stream().map(spaceId -> {
+        Space space = spaceService.getSpaceById(spaceId);
+        return space != null ? space.getGroupId() : null;
+      }).filter(Objects::nonNull).toList();
+
+      return memberships.stream().filter(membership -> spacesGroupId.stream().anyMatch(membership::contains)).toList();
+    }
+    return memberships;
   }
 
 }
