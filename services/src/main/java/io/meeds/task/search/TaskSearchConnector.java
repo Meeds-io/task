98bd79aca783f6d8eec1923d6f97037872380b4f
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-08-22 16:32:10 +0100

    fix: handle unsupported sort fields by returning default sorting query - MEED-9574 - Meeds-io/MIPs#212 (#532)

--- services/src/main/java/io/meeds/task/search/TaskSearchConnector.java
+++ services/src/main/java/io/meeds/task/search/TaskSearchConnector.java
@@ -228,7 +228,7 @@ public class TaskSearchConnector {
     String sortDirection = filter.getSortDirection();
     return switch (sortFiled) {
     case "date" -> SORTING_QUERY.replace("@sortField@", "lastUpdatedDate").replace("@sortOrder@", sortDirection);
-    default -> SORTING_QUERY.replace("@sortField@", sortFiled).replace("@sortOrder@", sortDirection);
+    default -> DEFAULT_SORTING_QUERY;
     };
   }
 }
