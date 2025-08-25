360563d2a5ff18125e9226c5e2faea49f6943464
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-08-25 09:45:51 +0100

    feat: Update Java and XML License Header - Meeds-io/meeds#3324 (#533) (#534)

--- services/src/main/java/org/exoplatform/task/dao/CommentHandler.java
+++ services/src/main/java/org/exoplatform/task/dao/CommentHandler.java
@@ -1,18 +1,20 @@
 /**
  * This file is part of the Meeds project (https://meeds.io/).
- * Copyright (C) 2022 Meeds Association
- * contact@meeds.io
+ *
+ * Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 package org.exoplatform.task.dao;
 
@@ -23,9 +25,6 @@ import org.exoplatform.commons.api.persistence.GenericDAO;
 import org.exoplatform.commons.utils.ListAccess;
 import org.exoplatform.task.domain.Comment;
 
-/**
- * @author <a href="mailto:tuyennt@exoplatform.com">Tuyen Nguyen The</a>.
- */
 public interface CommentHandler extends GenericDAO<Comment, Long> {
   ListAccess<Comment> findComments(long taskId);
 
--- services/src/main/java/org/exoplatform/task/dao/jpa/ProjectDAOImpl.java
+++ services/src/main/java/org/exoplatform/task/dao/jpa/ProjectDAOImpl.java
@@ -1,18 +1,20 @@
 /**
  * This file is part of the Meeds project (https://meeds.io/).
- * Copyright (C) 2022 Meeds Association
- * contact@meeds.io
+ *
+ * Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 package org.exoplatform.task.dao.jpa;
 
@@ -42,12 +44,6 @@ import org.exoplatform.task.dao.condition.SingleCondition;
 import org.exoplatform.task.domain.Project;
 import org.exoplatform.task.domain.UserSetting;
 
-/**
- * Created by The eXo Platform SAS
- * Author : Thibault Clement
- * tclement@exoplatform.com
- * 4/10/15
- */
 public class ProjectDAOImpl extends CommonJPADAO<Project, Long> implements ProjectHandler {
 
   private static final Logger LOG = Logger.getLogger("ProjectDAOImpl");
