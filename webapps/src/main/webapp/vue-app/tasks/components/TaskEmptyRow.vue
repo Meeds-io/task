cf005073dacebedb0a95ba8e696365dd4dc8ca94
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2026-01-26 10:54:36 +0100

    fix: Review the UI of tasks list placeholder - MEED-10159 - Meeds-io/meeds#4010 (#553)
    This change will add some adjustments to the UI of tasks list placeholder.

--- webapps/src/main/webapp/vue-app/tasks/components/TaskEmptyRow.vue
+++ webapps/src/main/webapp/vue-app/tasks/components/TaskEmptyRow.vue
@@ -6,6 +6,38 @@
       class="mb-2">
       fas fa-tasks
     </v-icon>
-    <p class="mt-2 mb-0 text-sub-title">{{ $t('label.start.adding.tasks') }}</p>
+    <p class="mt-2 mb-4">{{ $t('label.start.adding.tasks') }}</p>
+    <v-btn
+      id="addNewTaskButton"
+      :small="$root.isMobile"
+      color="primary"
+      outlined
+      @click="openTaskDrawer">
+      <span>
+        {{ $t('label.addTask') }}
+      </span>
+    </v-btn> 
   </div>
 </template>
+<script>
+export default {
+  props: {
+    project: {
+      type: String,
+      default: ''
+    },
+  },  
+  methods: {
+    openTaskDrawer() {
+      const defaultTask={
+        id: null,
+        status: {project: this.project},
+        priority: 'NONE',
+        description: '',
+        title: ''
+      };
+      this.$root.$emit('open-task-drawer', defaultTask);
+    },
+  }
+};
+</script>
