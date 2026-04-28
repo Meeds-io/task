fad9760db31b3c4bfa661396d2159d0022f88ad1
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2026-04-28 19:08:09 +0100

    feat: Move MCP Tools from AI Addon - EXO-86389

---
+++ services/src/main/java/io/meeds/task/mcp/model/TaskCommentModel.java
@@ -0,0 +1,55 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.task.mcp.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.meeds.mcp.server.tool.model.UserModel;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(value = Include.NON_EMPTY)
+public class TaskCommentModel {
+
+  @JsonProperty("task_comment_id")
+  private long      id;
+
+  @JsonProperty("task_id")
+  private long      taskId;
+
+  @JsonProperty("parent_comment_id")
+  private Long      parentCommentId;
+
+  private String    text;
+
+  private String    url;
+
+  @JsonProperty("created_date")
+  private String    createdDate;
+
+  private UserModel author;
+
+}
---
+++ services/src/main/java/io/meeds/task/mcp/model/TaskWithChangeLogModel.java
@@ -0,0 +1,47 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.task.mcp.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@JsonInclude(value = Include.NON_EMPTY)
+public class TaskWithChangeLogModel extends TaskModel {
+
+  @JsonProperty("change_logs")
+  private List<TaskChangeLog> changeLogs;
+
+  public TaskWithChangeLogModel(TaskModel model, List<TaskChangeLog> changeLogs) {
+    super(model);
+    this.changeLogs = changeLogs;
+  }
+
+}
